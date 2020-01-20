1e470dd5baaecff8a54ec8b18a5c3e38082a7ff2
Cody Murdock <warhammer4563@gmail.com>
2020-01-20 15:29:50 -0600

    completed Daily Journal Part 3

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,31 +1,53 @@
+
+
+const journalEntries = [
+    {
+        date: "01/07/2020",
+        concept: "Styling with CSS Selectors",
+        entry: "Learned how to style html pages using CSS",
+        mood: "Excited",
+    },
+    {
+        date: "01/08/2020",
+        concept: "How to Research Code",
+        entry: "Today I learned efficient ways to research code to solve problems in my coding",
+        mood: "Excited"
+    },
+    {
+        date: "01/09/2020",
+        concept: "Basic HTML Layout with Flexbox",
+        entry: "Today I learned the wondrous power of flexbox command",
+        mood: "Happy"
+    }
+];
+
 /*
-    Define the keys and value for a JavaScript object that
-    represents a journal entry about what you learned today
+    Purpose: To create, and return, a string template that
+    represents a single journal entry object as HTML
+
+    Arguments: journalEntry (object)
 */
-const journalEntry_1 = {
-    date: "01/07/2020",
-    concept: "Styling with CSS Selectors",
-    entry: "Learned how to style html pages using CSS",
-    mood: "Excited",
+const makeJournalEntryComponent = (journalEntry) => {
+    // Create your own HTML structure for a journal entry
+    return `<div class="entry">
+    <h1>${journalEntry.concept}</h1>
+    <p>${journalEntry.entry}<br>${journalEntry.date}</p>
+    </div>
+    `
 }
 
-const journalEntry_2 = {
-    date: "01/08/2020",
-    concept: "How to Research Code",
-    entry: "Today I learned efficient ways to research code to solve problems in my coding",
-    mood: "Excited"
-}
+const journalContainer = document.querySelector(".entryLog");
+/*
+    Purpose: To render all journal entries to the DOM
 
-const journalEntry_3 = {
-    date:"01/09/2020",
-    concept:"Basic HTML Layout with Flexbox",
-    entry:"Today I learned the wondrous power of flexbox command",
-    mood:"Happy"
-}
+    Arguments: entries (array of objects)
+*/
+const renderJournalEntries = (entries) => {
+    for (let i = 0; i < entries.length; i++) {
+        journalContainer.innerHTML += makeJournalEntryComponent(entries[i]);
+    }
+};
 
-const journalEntries = [];
-
-journalEntries.push(journalEntry_1, journalEntry_2, journalEntry_3);
-
-console.log(journalEntries);
+// Invoke the render function
+renderJournalEntries(journalEntries)
 
