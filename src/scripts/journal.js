e2337a6b8337ab589d898d57fe2ea11e6d418c1a
Cody Murdock <warhammer4563@gmail.com>
2020-01-27 08:45:09 -0600

    Daily Journal Part 5 completed

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,38 +1,5 @@
-const journalUrl = "http://localhost:3000/entries"
-
-fetch(journalUrl)
-    .then(entries => entries.json())
-    .then(parsedEntries => {
-        renderJournalEntries(parsedEntries)
-    })
-
 /*
-    Purpose: To create, and return, a string template that
-    represents a single journal entry object as HTML
-
-    Arguments: journalEntry (object)
+    Main application logic that uses the functions and objects
+    defined in the other JavaScript files.
 */
-const makeJournalEntryComponent = (journalEntry) => {
-    // Create your own HTML structure for a journal entry
-    return `
-    <div class="entry">
-        <h3>${journalEntry.concept}</h3>
-        <p>${journalEntry.entry}<br>${journalEntry.date}</p>
-    </div>
-    `
-}
-
-const journalContainer = document.querySelector(".entryLog");
-/*
-    Purpose: To render all journal entries to the DOM
-
-    Arguments: entries (array of objects)
-*/
-const renderJournalEntries = (entries) => {
-    for (let i = 0; i < entries.length; i++) {
-        journalContainer.innerHTML += makeJournalEntryComponent(entries[i]);
-    }
-};
-
-
-
+API.getJournalEntries().then(entriesDOM.renderJournalEntries)
